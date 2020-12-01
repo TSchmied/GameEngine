ec7aa2c1c9e8a198e1b6eeba117595f4a82cbdd1
Tomáš Schmied <tomas.schmied@kleech.cz>
2020-12-01 18:48:08 +0100

    Improved make script, added SDL2 + a test window

--- Make.py
+++ Make.py
@@ -12,6 +12,16 @@ class Mode(Enum):
     PROD = 2
 
 
+SETTINGS_COMPILER = "g++"
+SETTINGS_INCLUDE_PATHS = "-IC:\\mingw-w64\\dev_libs\\SDL2-2.0.12\\include\\SDL2 -IC:\\mingw-w64\\dev_libs\\SDL2_image-2.0.5\\include\\SDL2 -IC:\\mingw-w64\\dev_libs\\SDL2_mixer-2.0.4\\include\\SDL2"
+SETTINGS_LIBRARY_PATHS = "-LC:\\mingw-w64\\dev_libs\\SDL2-2.0.12\\lib -LC:\\mingw-w64\\dev_libs\\SDL2_image-2.0.5\\lib -LC:\\mingw-w64\\dev_libs\\SDL2_mixer-2.0.4\\lib"
+SETTINGS_EXECUTABLE_NAME = "kvejge_project.exe"  # TODO: MAKE CROSS PLATFORM
+SETTINGS_COMPILER_FLAGS = "-std=c++17"
+SETTINGS_COMPILER_DEV_FLAGS = ""
+SETTINGS_COMPILER_PROD_FLAGS = "-w"
+SETTINGS_LINKER_FLAGS = "-lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_mixer"
+
+
 def main(argv):
     mode = Mode.DEV
     for arg in argv:
@@ -35,16 +45,22 @@ def main(argv):
             if file not in cppsIgnored:
                 cpps.append("src/" + file)
 
+    compilerFlags = SETTINGS_COMPILER_FLAGS
+    if mode == Mode.DEV:
+        compilerFlags += " " + SETTINGS_COMPILER_DEV_FLAGS
+    elif mode == Mode.PROD:
+        compilerFlags += " " + SETTINGS_COMPILER_PROD_FLAGS
+
     outputPath = os.getcwd() + "/builds/" + mode.name + "/"
-    execName = "kvejge_project.exe"  # TODO: MAKE CROSS PLATFORM
 
     if not os.path.isdir(outputPath):
         os.mkdir(outputPath)
 
     print("\nCompiling...")
-    os.system('g++ ' + ' '.join(cpps) + ' -o ' + outputPath + execName)
+    os.system(SETTINGS_COMPILER + " " + ' '.join(cpps) + " " + SETTINGS_INCLUDE_PATHS + " " + SETTINGS_LIBRARY_PATHS +
+              " " + compilerFlags + " " + SETTINGS_LINKER_FLAGS + ' -o ' + outputPath + SETTINGS_EXECUTABLE_NAME)
     print("\nRunning...")
-    os.system(outputPath + execName)
+    os.system(outputPath + SETTINGS_EXECUTABLE_NAME)
 
 
 if __name__ == '__main__':
--- src/TestApp.cpp
+++ src/TestApp.cpp
@@ -1,8 +1,32 @@
+#include <SDL.h>
+
 #include "Logger.h"
 
 int main(int argc, char *args[])
 {
     Kvejge::Logger logger;
     logger.logSomeStuff();
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow("KVEJGE TEST", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    SDL_Surface *surface = SDL_GetWindowSurface(window);
+
+    bool shouldQuit = false;
+    SDL_Event e;
+    while (!shouldQuit)
+    {
+        while (SDL_PollEvent(&e) != 0)
+        {
+            if (e.type == SDL_QUIT)
+            {
+                shouldQuit = true;
+            }
+        }
+        SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 255, 0, 103));
+        SDL_UpdateWindowSurface(window);
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
